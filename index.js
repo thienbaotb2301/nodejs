6944da85b694388eb6ac43e4a59ba295bab1fde8
Đường Thiên Bảo <thienbao@macbook.local>
2019-12-31 09:57:55 +0700

    Add feature view & create id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
+var shortid = require('shortid');
 
 var adapter = new FileSync('db.json');
 var db = low(adapter);
@@ -32,7 +33,7 @@ app.get('/users', function(req, res) {
 
 app.get('/users/search', function(req, res) {
     var q = req.query.q;
-    var matchedUsers = users.filter(function(user) {
+    var matchedUsers = db.get('users').value().filter(function(user) {
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
     res.render('users/index', {
@@ -44,7 +45,19 @@ app.get('/users/create', function(req, res) {
     res.render('users/create');
 });
 
+app.get('/users/:id', function(req, res) {
+    var id = req.params.id;
+
+    var user = db.get('users').find({ id: id }).value();
+    
+    res.render('users/view', {
+        user: user
+    });
+});
+
 app.post('/users/create', function(req, res) {
+    req.body.id = shortid.generate();
+
     db.get('users').push(req.body).write();
     res.redirect('/users');
 });
