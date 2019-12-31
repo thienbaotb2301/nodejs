ec9e70a7d86e3a29bca92ea1c746782932ac2250
Đường Thiên Bảo <thienbao@macbook.local>
2019-12-31 11:10:20 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var shortid = require('shortid');
 
-var adapter = new FileSync('db.json');
-var db = low(adapter);
-
-db.defaults({ users: [] })
-  .write();
+var userRoutes = require('./routes/user.route');
 
 var port = 3000;
+
+var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -25,42 +19,7 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/users', function(req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/search', function(req, res) {
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter(function(user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/index', {
-        users: matchedUsers
-    });
-});
-
-app.get('/users/create', function(req, res) {
-    res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res) {
-    var id = req.params.id;
-
-    var user = db.get('users').find({ id: id }).value();
-    
-    res.render('users/view', {
-        user: user
-    });
-});
-
-app.post('/users/create', function(req, res) {
-    req.body.id = shortid.generate();
-
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-});
+app.use('/users', userRoutes);
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
