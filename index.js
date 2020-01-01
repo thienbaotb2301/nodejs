946e60b5fa29e511191a5de4394dc45249a22d64
Đường Thiên Bảo <thienbao@macbook.local>
2020-01-01 14:51:06 +0700

    Signed Cookie

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('abc'));
 
 app.use(express.static('public'));
 
